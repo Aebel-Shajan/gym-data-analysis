2cc85e5ad90d7efbfe9e20340c6913fee9f6427d
aebel-shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-06-05 21:27:21 +0100

    Move main.py back outside

--- gym_data_analysis/main.py
+++
@@ -1,7 +0,0 @@
-from gym_data_analysis import data_processing
-
-def main():
-    data_processing.process_data()
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,7 @@
+from gym_data_analysis.data_processing import process_data
+
+def main():
+    process_data()
+
+if __name__ == "__main__":
+    main()
