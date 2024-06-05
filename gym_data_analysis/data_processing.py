54496c1f561a346504f6f6d16aa4e437736d9be4
aebel-shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-06-05 21:29:14 +0100

    Add code to read in csv

--- gym_data_analysis/data_processing.py
+++ gym_data_analysis/data_processing.py
@@ -1,4 +1,12 @@
+import pandas as pd
+import yaml
 
 def process_data():
     print("Starting processing data")
+    config = None
+    with open("config.yaml") as f:
+        config = yaml.safe_load(f)
+    
+    workout_data = pd.read_csv(config["input_data"], delimiter=";")
+    print(workout_data.head())
     
