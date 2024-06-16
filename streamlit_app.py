03e77a4432606ae3d3b91cab5dead7551fabb220
aebel-shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-06-16 15:12:56 +0100

    Add instructions and sample data to streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import gym_data_analysis.data_processing as analysis
 
+st.image("./thumbnail.png")
+st.markdown("# Strong app data analysis")
 
-st.write("Strong app data analysis")
+st.markdown(
+    """
+        To get data: 
+        1. Open strong app
+        2. Click profile icon at bottom
+        3. Click settings icon at top right
+        4. Scroll down to "general" section
+        5. Click export data to download csv data
+        
+        or just use the sample data here for a demo:
+    """)
 
-uploaded_file = st.file_uploader("Upload strong data csv", type="csv")
+sample_data = pd.read_csv(
+    "./data/input/strong8580138242478526790.csv",
+    delimiter=";",
+    parse_dates=['Date']
+    ).to_csv().encode("utf-8")
+st.download_button(
+    label="Download sample data",
+    data=sample_data,
+    file_name="strong_sample_data.csv",
+    mime="text/csv",
+)
+
+uploaded_file = st.file_uploader("Upload csv file from strong app here:", type="csv")
+
 
 if uploaded_file is None:
     exit()
