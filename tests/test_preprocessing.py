a8ac297311a87c4b81631c2b38a8ab3a85cb5db8
aebel <67755450+Aebel-Shajan@users.noreply.github.com>
2024-06-18 15:35:13 +0100

    Add test_convert_weights_to_metric

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,6 +1,7 @@
 import pytest
 import gym_data_analysis.preprocessing as preprocessing
 import numpy as np
+import pandas as pd
 
 
 @pytest.mark.parametrize("duration, expected_output", [
@@ -12,3 +13,20 @@ import numpy as np
 def test_parse_duration(duration, expected_output):
     actual_output = preprocessing.parse_duration(duration)
     assert actual_output == expected_output
+
+
+
+@pytest.mark.parametrize(
+    "input_row, expected_weight, expected_unit",
+    [
+        (pd.Series({"Weight": 220.5, "Weight Unit": "lbs"}), 220.5 / 2.205, "kg"),
+        (pd.Series({"Weight": 100, "Weight Unit": "kg"}), 100, "kg"),
+        (pd.Series({"Weight": 150, "Weight Unit": "lbs"}), 150 / 2.205, "kg"),
+        (pd.Series({"Weight": 75, "Weight Unit": "kg"}), 75, "kg")
+    ]
+)
+def test_convert_weights_to_metric(input_row, expected_weight, expected_unit):
+    actual_output = preprocessing.convert_weights_to_metric(input_row)
+    assert actual_output["Weight"] == expected_weight
+    assert actual_output["Weight Unit"] == expected_unit
+    
