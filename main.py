bd0b0d27c477aba8a1e6a49df95e58840c6a8ac0
aebel <67755450+Aebel-Shajan@users.noreply.github.com>
2024-06-18 18:27:01 +0100

    Refactor main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from gym_data_analysis.preprocessing import preprocess_strong_csv
+import yaml
 
 def main():
-    preprocess_strong_csv()
+    config = None
+    with open("config.yaml") as f:
+        config = yaml.safe_load(f)
+
+    #Preprocessing
+    raw_df = preprocess_strong_csv(config["input_data"])
+    print(raw_df)
 
 if __name__ == "__main__":
     main()
